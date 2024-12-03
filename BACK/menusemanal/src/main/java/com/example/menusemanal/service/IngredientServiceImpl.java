431bdd33bad963e08d33a8e93943178f5ce0a334
lora krasimi <loraadova@gmail.com>
2024-12-03 12:55:50 +0100

    Controller Method Service Method Repository Season, SeasonHasIngredientRepository, DTO's

--- BACK/menusemanal/src/main/java/com/example/menusemanal/service/IngredientServiceImpl.java
+++ BACK/menusemanal/src/main/java/com/example/menusemanal/service/IngredientServiceImpl.java
@@ -10,7 +10,6 @@ import com.example.menusemanal.persistance.SeasonHasIngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
